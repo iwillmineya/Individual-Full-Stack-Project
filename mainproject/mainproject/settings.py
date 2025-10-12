4c7985d8146738fd557d8324509b2e5658e4f260
Tyler Pendleton <tylerprivate04@gmail.com>
2025-10-12 17:00:04 +0100

    repeat

--- mainproject/mainproject/settings.py
+++ mainproject/mainproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os;
 import dj_database_url
+import mainproject.settings
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
