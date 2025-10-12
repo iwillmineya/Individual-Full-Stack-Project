9e9173138a5a5f51d29cdc862a65a45265c427fe
Tyler Pendleton <tylerprivate04@gmail.com>
2025-10-12 16:36:48 +0100

    r

--- mainproject/mainproject/settings.py
+++ mainproject/mainproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "gameyap/static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "gameyap/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
