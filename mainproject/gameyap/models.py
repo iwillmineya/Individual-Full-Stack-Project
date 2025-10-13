ea7b4dea0eb3513f74963f2b616fe1e360a6d420
Tyler Pendleton <tylerprivate04@gmail.com>
2025-10-13 06:50:51 +0100

    Remove duplicate Game model definition to fix ValueError

--- mainproject/gameyap/models.py
+++ mainproject/gameyap/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# ...existing code...
-
 class Game(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
@@ -13,7 +11,6 @@ class Game(models.Model):
 
     def __str__(self):
         return self.title
-
 
 class GameMedia(models.Model):
     IMAGE = 'image'
@@ -27,30 +24,14 @@ class GameMedia(models.Model):
     file = models.FileField(upload_to='game_media/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-
-from django.db import models
-from django.contrib.auth.models import User
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    favorite_games = models.ManyToManyField('Game', blank=True, related_name='fans')
+    favorite_games = models.ManyToManyField(Game, blank=True, related_name='fans')
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
-
-class Game(models.Model):
-    title = models.CharField(max_length=100)
-    genre = models.CharField(max_length=50)
-    release_date = models.DateField()
-    description = models.TextField(blank=True)
-    likes = models.PositiveIntegerField(default=0)
-    dislikes = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.title
-
 
 class Vote(models.Model):
     LIKE = 'like'
