d6ade837e0efbac8e849abbeac49461899c7f5d1
Tyler Pendleton <tylerprivate04@gmail.com>
2025-10-13 04:20:35 +0100

    Fix DJANGO_SETTINGS_MODULE for Heroku

--- mainproject/mainproject/wsgi.py
+++ mainproject/mainproject/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 """
@@ -27,6 +27,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
