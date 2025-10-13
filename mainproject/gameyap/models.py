6cbe822266f096691090bee0f2328b00bb4115c1
Tyler Pendleton <tylerprivate04@gmail.com>
2025-10-13 05:44:09 +0100

    Add UserProfile model for user profiles

--- mainproject/gameyap/models.py
+++ mainproject/gameyap/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    favorite_games = models.ManyToManyField('Game', blank=True, related_name='fans')
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
 # Create your models here.
 
